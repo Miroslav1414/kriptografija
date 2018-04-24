f33aec25c6d90e7bb14d15640eadc54c5d017d62
Miroslav Mandic <miroslav1414@gmail.com>
2018-04-24 09:37:24 +0200

    obrni red

--- src/kripto/Helper.java
+++ src/kripto/Helper.java
@@ -29,4 +29,33 @@ public class Helper {
         return rez;
     }
     
+    public static byte[] stringToByte (String str){
+        byte[] rez = new byte[str.toCharArray().length];
+        int brojac = 0;
+        for (char c : str.toCharArray()){
+            rez[brojac++] = (byte)c;
+        }
+        return rez;
+    }
+    
+    public static String byteToString(byte [] niz ){
+        String rez = "";
+        for(byte a : niz)
+            rez += (char)(a);
+        return rez;
+    }
+    
+    public static void main (String args []){
+        byte a = (byte) 72;
+        byte [] aa = {(byte)-255};
+        System.out.println((char)255);
+        System.out.println(Byte.toString(a) + " " + aa[0]);
+        while(a != (byte)0)
+        {
+            System.out.print(a%2);
+            a/=2;
+        }
+        System.out.println(byteToString(aa));
+        
+    }
 }
